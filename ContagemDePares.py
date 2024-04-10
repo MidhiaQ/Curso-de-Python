a79bfa3e4a1232d184f730d9bd186ed8ec95f712
MidhiaQ <queirozmidhia9@gmail.com>
2024-04-09 21:02:22 -0300

    módulo 3

---
+++ ContagemDePares.py
@@ -0,0 +1,4 @@
+#Pares entre 1 e 50
+for num in range(2, 51, 2):
+    #print('.', end='')
+    print(num, end=' ')
---
+++ ContagemRegressiva.py
@@ -0,0 +1,5 @@
+from time import sleep
+for num in range(10, -1, -1):
+    print(num)
+    sleep(1)
+print('FIM')   
